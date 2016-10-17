65484d2939ca838c105793d74ce1bd07e14d56c0
appledix <dxl2010@mail.ru>
2016-10-18 00:05:55 +0300

    Загружен первоначальный вариант.

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,48 @@
 import vk
+import itertools
+import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = 5672090
 
 
 def get_user_login():
-    pass
-
+    while True:
+        login = input("Login: ")
+        if login != "": break
+    return login
 
 def get_user_password():
-    pass
-
+    while True:
+        password = getpass.getpass(prompt='Password: ', stream=None)
+        if password != "": break
+    return password
 
 def get_online_friends(login, password):
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
-        user_password=password,
-    )
+        user_password=password
+        )
     api = vk.API(session)
-    # например, api.friends.get()
-
+    friends = api.friends.get(fields="online")
+    online_friends = list(itertools.filterfalse(lambda friend: friend['online'] != 1, friends))
+    return online_friends
 
 def output_friends_to_console(friends_online):
-    pass
+    print("Друзья онлайн:")
+    for friend in friends_online:
+        print(friend['first_name'], friend['last_name'])
+    return
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    while True:
+        login = get_user_login()
+        password = get_user_password()
+        try:
+            friends_online = get_online_friends(login, password)
+        except Exception:
+            continue
+        else:
+            output_friends_to_console(friends_online)
+            break
